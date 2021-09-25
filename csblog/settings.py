033e127159a31fe6e0cbebee77f07f4263cc7080
DriftingDane <driftingdanel@gmail.com>
2021-09-25 10:53:49 +0300

    Added Django Sites model and updated copy_url function - created custom add_view function to redirect to custom add page

--- csblog/settings.py
+++ csblog/settings.py
@@ -102,9 +102,9 @@ if DEBUG:
 	DATABASES = {
 		'default': {
 			'ENGINE': 'django.db.backends.mysql',
-			'NAME': 'customblog',
-			'USER': 'django',
-			'PASSWORD': 'Django76',
+			'NAME': '',
+			'USER': '',
+			'PASSWORD': '',
 			'HOST': '127.0.0.1',
 			'PORT': '3306',
 		}
@@ -237,5 +237,5 @@ EMAIL_HOST_USER = 'driftingdane@gmail.com'
 EMAIL_HOST_PASSWORD = 'yhshkjxxqoamqpgx'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
